23f62a22dc25a83869933c2f9ceb67dc6cc428a8
Иван Пенчев <i.pen4ev@gmail.com>
2015-08-10 16:33:58 -0700

    Модул 02,Видео 07 - Работа с масиви

---
+++ Modul02/Table Project/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Table_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string []row = new string[3];
+            row[0] = "Ред 1";
+            row[1] = "Ред 2";
+            row[2] = "Ред 3";
+
+            Console.WriteLine("Масив ROW: " + row[0] + ", " + row[1] + ", " + row[2] + "\n ");
+
+            string[] parse = "1,2,3,4,5,6,7,8,9".Split(',');
+            Console.WriteLine("Броят на масива:\n" + "1,2,3,4,5,6,7,8,9\n\ne:"+parse.Length);
+
+            string list1 = string.Join(";", parse);
+            Console.WriteLine("Новият стринг е:\n"+list1+"\n\n");
+        }
+    }
+}
