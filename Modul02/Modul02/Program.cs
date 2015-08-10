787f77872f6ca71ad85e69c883fe9d376abfa0a3
Иван Пенчев <i.pen4ev@gmail.com>
2015-08-09 23:44:42 -0700

    Modul 02. Видео 01 - Добавяне на коментари

---
+++ Modul02/Modul02/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Modul02
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			//дефиниране на променливи
+
+			//Събиране на Числа
+
+			//Писане в конзола
+
+
+		}
+	}
+}
