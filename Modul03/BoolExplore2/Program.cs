a86e3d0dd586681bd117e3445c8db3d00e4d4e60
Иван Пенчев <i.pen4ev@gmail.com>
2015-08-13 15:00:06 -0700

    Модул 03, Видео  - Примери с променливи BoolExplore - 2

---
+++ Modul03/BoolExplore2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoolExplore2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] _i = new int[] { 4, 3, 2 };
+
+            int _ui = 0; 
+
+            string _input = "0";
+
+            Console.Write("Моля въведете индекс: ");
+            _input = Console.ReadLine();
+
+            bool _check01 = int.TryParse(_input, out _ui);
+
+            _ui = (_ui <= _i.Length) ? _ui : 3;
+            _ui = (_ui > 0) ? _ui : 1;
+
+
+
+
+
+            if (_check01)
+            {
+                Console.WriteLine(_i[_ui - 1]);
+            }
+
+        }
+    }
+}
