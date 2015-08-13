a2a5f2cb51851ffe7e059ca2ad02847b85978ec7
Иван Пенчев <i.pen4ev@gmail.com>
2015-08-13 14:48:13 -0700

    Модул 03, Видео 07 - Примери с променливи BOOL - 2

--- Modul03/BoolExplore/Program.cs
+++ Modul03/BoolExplore/Program.cs
@@ -16,7 +16,16 @@ namespace BoolExplore
 
             int [] _i = new int[]{4,3,2};
 
-            int _ui = Convert.ToInt32(Console.ReadLine());
+            int _ui = 0; // Convert.ToInt32(Console.ReadLine());
+
+            string _input = "0";
+
+            Console.Write("Моля въведете индекс: ");
+            _input = Console.ReadLine();
+
+            bool _check01 = int.TryParse(_input, out _ui);
+            bool _check02 = _ui <= _i.Length;
+            bool _check03 = _ui > 0;
 
             //bool _check01 = _ui <= _i.Length;
             //bool _check02 = _ui > 0;
@@ -30,25 +39,44 @@ namespace BoolExplore
             //Console.WriteLine("Логическо ИЛИ: "+_exp02.ToString());
             //Console.WriteLine("Отрицание на "+ _check01.ToString() + "; " + _exp03); 
 
+            if (_check01 && _check02 && _check03)
+            {
+                Console.WriteLine(_i[_ui -1]);
+            }
 
-            if (_ui >0 && _ui <= _i.Length)
+            if (!_check01)
             {
-                
-                Console.WriteLine(_i [_ui -1]);
-            }
-            else
+                Console.WriteLine("Индекса трябва да бъде цяло число:\n\n\n");
+
+            }if (!_check02)
             {
-                if (_ui > _i.Length)
-                {
-                    Console.WriteLine("Индекса е прекалено голям:\n\n\n");
-                }
-                
-                if (_ui <= 0)
-                {
-                    Console.WriteLine("Индекса е прекалено малък:\n\n\n");
-                }
-              //  Console.WriteLine("Невалиден Индекс\n\n\n");
+                Console.WriteLine("Индекса е прекалено голям:\n\n\n");
+
+            }if (!_check03 && _check01)
+            {
+                Console.WriteLine("Индекса е прекалено малък:\n\n\n");
             }
+
+
+
+            //if (_ui >0 && _ui <= _i.Length)
+            //{
+                
+            //    Console.WriteLine(_i [_ui -1]);
+            //}
+            //else
+            //{
+            //    if (_ui > _i.Length)
+            //    {
+            //        Console.WriteLine("Индекса е прекалено голям:\n\n\n");
+            //    }
+                
+            //    if (_ui <= 0)
+            //    {
+            //        Console.WriteLine("Индекса е прекалено малък:\n\n\n");
+            //    }
+            //  //  Console.WriteLine("Невалиден Индекс\n\n\n");
+            //}
 
 
 
