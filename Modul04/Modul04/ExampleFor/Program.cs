d41137db4dc8b903384bdacef2b84681e54ec27b
Иван Пенчев <i.pen4ev@gmail.com>
2015-08-14 16:55:06 -0700

    Модул 04, Видео 02 -Примери с For Цикъл

---
+++ Modul04/Modul04/ExampleFor/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] _table = {"Иван", "Гошо", "Гергана"};
+
+            //Антетка на таблица
+            Console.WriteLine("Име");
+            Console.WriteLine("===============");
+
+            //Тяло на таблица
+            for (int i = 0; i < _table.Length; i ++)
+            {
+                //Код на Цикъка
+                Console.WriteLine(_table[i]);
+            }
+        }
+    }
+}
