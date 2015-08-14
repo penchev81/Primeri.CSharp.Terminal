c7a6a519b82ccec010f1b93f85e1661997685892
Иван Пенчев <i.pen4ev@gmail.com>
2015-08-14 16:44:35 -0700

    Модул 04, Видео 01 - FOR цикъл

---
+++ Modul04/Modul04/for/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @for
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 5; i += 2)
+            {
+                //Код на Цикъка
+                Console.WriteLine(i.ToString());
+            }
+        }
+    }
+}
