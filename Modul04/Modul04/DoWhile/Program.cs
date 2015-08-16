acdd7480863b806343fc5494b528787f6f50a06d
Иван Пенчев <i.pen4ev@gmail.com>
2015-08-15 23:30:40 -0700

    Модул 04, Видео 06 - ExampleDoWhile

--- Modul04/Modul04/DoWhile/Program.cs
+++ Modul04/Modul04/DoWhile/Program.cs
@@ -28,15 +28,7 @@ namespace DoWhile
 
 
             int i = 0;
-            //while (i < _table.Length/3)
-            //{
-            //    Console.WriteLine
-            //        (_table[i, 0].PadRight(12) +
-            //         _table[i, 1].PadRight(12) +
-            //         _table[i, 2].PadRight(12)
-            //        );
-            //    i++;
-            //}
+            
             do
             {
             Console.WriteLine
---
+++ Modul04/Modul04/ExamplesDoWhile/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamplesDoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Дефиниции
+            string _userinput = "";
+
+            //Потребителски вход
+            do
+            {
+                Console.Write("?");
+                _userinput = Console.ReadLine();
+            } while (_userinput != "exit");
+
+        }
+    }
+}
