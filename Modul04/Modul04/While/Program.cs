d7351e2f6bfe0d2bed4a5aabaae38c90d6403497
Иван Пенчев <i.pen4ev@gmail.com>
2015-08-15 22:57:31 -0700

    Модул 04, Видео 04 - While

---
+++ Modul04/Modul04/While/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[,] _table =
+            {
+                {"Иван",    "Георгиев",     "19"},
+                {"Васил",   "Иванов",       "23"},
+                {"Милен",   "Георгиев",     "58"},
+                {"Цветан",  "Бориславов",   "14"},
+                {"Гергана", "Василева",     "25"},
+                {"Ивана",   "Христова",     "21"}
+
+            };
+
+            //Антетка на таблица
+            Console.WriteLine("Име".PadRight(12) + "Фамилия".PadRight(12) + "Години".PadRight(12));
+            Console.WriteLine("=================================");
+
+
+            int i = 0;
+            while (i<_table.Length /3)
+            {
+                Console.WriteLine
+                    (_table[i,0].PadRight(12) + 
+                     _table[i,1].PadRight(12) +
+                     _table[i,2].PadRight(12));
+                i++;
+            }
+        }
+    }
+}
